c0a320d7a4006498e9a01a83f8a50a6aff184bba
zhaowebb <zhaowebb@gmail.com>
2017-08-03 20:10:07 -0400

    modification to server_address

--- BookmarkServer.py
+++ BookmarkServer.py
@@ -106,6 +106,7 @@ class Shortener(http.server.BaseHTTPRequestHandler):
                 "Couldn't fetch URI '{}'. Sorry!".format(longuri).encode())
 
 if __name__ == '__main__':
-    server_address = ('PORT', 8000)
+    port = int(os.environ.get('PORT', 8000))   # Use PORT if it's there.
+    server_address = ('', port)
     httpd = http.server.HTTPServer(server_address, Shortener)
     httpd.serve_forever()
