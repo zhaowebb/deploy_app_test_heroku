5f2e64d6f1f0308d7845e6c16cd3e0bed6e45546
zhaowebb <zhaowebb@gmail.com>
2017-08-04 21:17:33 -0400

    enable to handle concurrency

--- BookmarkServer.py
+++ BookmarkServer.py
@@ -5,6 +5,8 @@ import os
 import http.server
 import requests
 from urllib.parse import unquote, parse_qs
+import threading
+from socketserver import ThreadingMixIn
 
 memory = {}
 
@@ -26,7 +28,8 @@ form = '''<!DOCTYPE html>
 {}
 </pre>
 '''
-
+class ThreadHTTPServer(ThreadingMixIn, http.server.HTTPServer):
+    "This is an HTTPServer that supports thread-based concurrency."
 
 def CheckURI(uri, timeout=5):
     '''Check whether this URI is reachable, i.e. does it return a 200 OK?
@@ -106,7 +109,7 @@ class Shortener(http.server.BaseHTTPRequestHandler):
                 "Couldn't fetch URI '{}'. Sorry!".format(longuri).encode())
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8000))   # Use PORT if it's there.
+    port = int(os.environ.get('PORT', 8000))
     server_address = ('', port)
-    httpd = http.server.HTTPServer(server_address, Shortener)
+    httpd = ThreadHTTPServer(server_address, Shortener)
     httpd.serve_forever()
